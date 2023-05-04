ac7ba57742fc146318c57b98dace8bca00d0cae1
GOTLERH <gersonmiranda94@yahoo.es>
2023-05-03 21:37:03 -0600

    Files deleted - Alejandro Agregó el diseño.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import "./assets/styles/sakura.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./components/navbar/Navbar";
-import Album from "./components/album/Album";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<Navbar />
-		<Album />
+
 	</React.StrictMode>
 );
